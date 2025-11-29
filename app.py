96511c9826027874a2883fe06be133c3abc0f831
ShravanSM <shravanmaharajpet8@gmail.com>
2025-11-29 09:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import streamlit as st
-from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
 from langchain.schema import HumanMessage, SystemMessage
 
 # Load API key
